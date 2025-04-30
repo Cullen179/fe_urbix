c233c9d1d5473686bea274d1c43c104d38da7750
Cullen179 <itslamemail@gmail.com>
2025-04-30 10:04:12 +0700

    feat: test page to view language support

---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,20 @@
+import {useTranslations} from 'next-intl';
+import {Link} from '@/i18n/navigation';
+import { use } from 'react';
+import { setRequestLocale } from 'next-intl/server';
+ 
+export default function Home({ params }: { params: { locale: string } }) {
+
+  const { locale } = params;
+  
+  setRequestLocale(locale);
+
+  const t = useTranslations('HomePage');
+  
+  return (
+    <div>
+      <h1>{t('title')}</h1>
+      <Link href="/about">{t('about')}</Link>
+    </div>
+  );
+}
